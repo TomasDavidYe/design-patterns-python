d6076861550e6ba50830cea5381cf838ddd408fa
Tomas Ye <tomaye@amazon.com>
2021-05-03 09:48:55 +0200

    [STATISTICS] Adding F-Scores

--- statistics/f_score.py
+++ statistics/f_score.py
@@ -18,6 +18,7 @@ true_negative = 98
 accuracy = (true_positive + true_negative) / total_num_observations  # = (0 + 98 / 100) = 0.98
 precision = true_positive / (true_positive + false_positive)  # = 0 / (0 + 0) = NaN
 recall = true_positive / (true_positive + true_negative)  # = 0 / (0 + 98) = 0
+f_score = (2 * precision * recall) / (precision + recall)  # NaN
 
 # Smarter model which got some rare observations right
 total_num_observations = 100
@@ -31,6 +32,7 @@ true_negative = 96
 accuracy = (true_positive + true_negative) / total_num_observations  # = (1 + 96 / 100) = 0.97
 precision = true_positive / (true_positive + false_positive)  # = 1 / (1 + 2) = 0.33
 recall = true_positive / (true_positive + true_negative)  # = 1 / (1 + 96) = 0.0103
+f_score = (2 * precision * recall) / (precision + recall)  # = ~0.02
 
 
 
@@ -49,4 +51,4 @@ true_negative = 0
 accuracy = (true_positive + true_negative) / total_num_observations  # = (2 / 100) = 0.02
 precision = true_positive / (true_positive + false_positive)  # = 2 / (2 + 98) = 0.02
 recall = true_positive / (true_positive + true_negative)  # = 2 / (2 + 0) = 1.0
-
+f_score = (2 * precision * recall) / (precision + recall)  # = ~0.04
